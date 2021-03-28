a47e4b103de5325001183de4110e17271eae58bf
gaj0001 <a.gajic@hotmail.com>
2021-03-29 10:22:04 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jasmine-in-angular-6fie3d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/jasmine-in-angular-6fie3d)
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,5 @@
+describe('Testing tests', () => {
+
+  it('should succeed', () => expect(true).toEqual(true));
+  it('should fail', () => expect(true).toEqual(false));
+});
